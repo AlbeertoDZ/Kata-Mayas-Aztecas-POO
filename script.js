7b37dc992db413a02029543c15f03ff28d98c4c8
AlbeertoDZ <albertodiezalcobendas@gmail.com>
2025-04-02 13:16:34 +0200

    fix/parametros daban error

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ class Warrior {
     }
 
     attack(){
-        this.power;
+        return this.power;
     }
 
     defend(damage) {
-        this.life - damage;
+        this.life -= damage;
+        console.log(`has sufrido un daño de ${damage}, te queda ${this.life} de vida`)
     }
 }
 
@@ -21,7 +22,7 @@ class Maya extends Warrior {
     }
 
     drinkColacao(){
-        this.power = power + 10;
+        this.power += 10;
     }
 }
 
@@ -30,15 +31,22 @@ class Aztec extends Warrior {
         super(life,power)
     }
     drinkNesquik(){
-        this.life = life + 10;
+        this.life += 10;
     }
 }
 
-let peleadorMaya = new Maya(50, 10);
-let peleadorAzteca = new Aztec(50, 10);
+let peleadorMaya = new Maya(200, 30);
+let peleadorAzteca = new Aztec(200, 20);
+
 peleadorAzteca.drinkNesquik();
 peleadorMaya.drinkColacao();
+
+const damageMaya = peleadorMaya.attack();
+
 peleadorMaya.attack();
-peleadorAzteca.defend();
+peleadorAzteca.defend(damageMaya);
+
+const damageAzteca = peleadorAzteca.attack();
+
 peleadorAzteca.attack();
-peleadorMaya.defend(); 
+peleadorMaya.defend(damageAzteca); 
